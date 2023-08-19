80a7d099ac75d398d713195ce8aaab3ef155900f
JossePerez <jose.perez18@tectijuana.edu.mx>
2023-08-18 23:30:41 -0700

    Conexion con la base de datos e instalacion de nuevas dependencias

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port= process.env.port || 9000;
@@ -7,5 +9,11 @@ const port= process.env.port || 9000;
 app.get("/", (req, res)=> {
     res.send("Welcome to my API");
 }); 
+
+//MongoDB conection 
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to mongoDB Atlas'))
+    .catch((error)=> console.error(error));    
 
 app.listen(port, () => console.log('Servidor listo en el puerto: ', port));
